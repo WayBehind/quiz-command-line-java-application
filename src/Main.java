44d139e8a34c70ad0a8357ab7c982f2dcc2d52bb
WayBehind <mgromoss@gmail.com>
2025-07-15 17:25:20 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // TODO: Class Question has base variables and methods.
+        // TODO: Other Question Classes inherit from Question and modify/add for their purposes.
+        // TODO: Enum with description containing in Maps String of actual quiz question and answers.
+        // TODO: Class Quiz contains and/or uses all of the above to start, run and end the quiz.
+
+    }
+}
