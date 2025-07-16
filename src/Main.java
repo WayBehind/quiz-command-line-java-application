deb80a8d25d2b98cd6e2f42ff8385084e3daaee9
WayBehind <mgromoss@gmail.com>
2025-07-16 20:01:22 +0200

    cut-out-bloat-aka-everything

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ public class Main {
 
         // TODO: Class Question has base variables and methods.
         // TODO: Other Question Classes inherit from Question and modify/add for their purposes.
-        // TODO: Enum with description containing in Maps String of actual quiz question and answers.
         // TODO: Class Quiz contains and/or uses all of the above to start, run and end the quiz.
-
     }
 }
